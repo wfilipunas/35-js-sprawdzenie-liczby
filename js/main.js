b269f68d4f483bbfbb05cef6e286c7fd865315d9
wfilipunas <wfilipunas@gmail.com>
2021-05-20 22:28:54 +0200

    Sprawdzenie liczby

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function sprawdz(n) {
+    if (n >= 100 && n <= 200) {
+        console.log('Liczba znajduje się w przedziale');
+    } else {
+        console.log('Liczba nie znajduje się w przedziale');
+    }
+}
+
+sprawdz(101);
